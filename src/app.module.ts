8b602595be0f3eb2a934bed476125e285d2ddfbc
likhanna <khlr@list.ru>
2024-11-17 18:55:47 +0500

    feat: add typeorm and db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,18 @@ import { TrackModule } from './track/track.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { FavsModule } from './favs/favs.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmAsyncConfig } from './db/typeorm-config';
 
 @Module({
-  imports: [UserModule, TrackModule, ArtistModule, AlbumModule, FavsModule],
+  imports: [
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    UserModule,
+    TrackModule,
+    ArtistModule,
+    AlbumModule,
+    FavsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
